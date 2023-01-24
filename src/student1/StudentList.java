e82a5af3bc6196f91e133ca2dd38001dedbac7fa
simran242003 <123481397+simran242003@users.noreply.github.com>
2023-01-24 10:12:42 -0600

    Added date for pull command

--- src/student1/StudentList.java
+++ src/student1/StudentList.java
@@ -1,6 +1,7 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ Date:24 jan 2023 for pull command
  */
 package student1;
 
