ad391b01741e0817870995c5d2ba6d1887fedde1
Simran <simran33@sheridancollege.ca>
2023-01-24 11:34:38 -0600

    Added new part time student class

---
+++ src/partTmeStudent.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Simran
+ */
+public class partTmeStudent extends Student1 {
+    private int numOfCourses;
+
+    public partTmeStudent(int numOfCourses, String studentName) {
+        super(studentId,studentName);
+        this.numOfCourses = numOfCourses;
+    }
+
+    public int getNumOfCourses() {
+        return numOfCourses;
+    }
+
+    public void setNumOfCourses(int numOfCourses) {
+        this.numOfCourses = numOfCourses;
+    }
+    
+    
+    
+}
